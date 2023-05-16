63d36f7ead24dce55c62c75b6ff7307fbcf7ee8a
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-05-16 19:58:29 +0200

    added search func & disp basic info

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const searchInputEl = document.getElementById('search-input');
+const searchButtonEl = document.getElementById('search-button');
+const townEl = document.getElementById('town');
+const countryEl = document.getElementById('country');
+const temperatureEl = document.getElementById('temperature');
+const conditionsEl = document.getElementById('conditions');
+const windEl = document.getElementById('wind');
+const pressureEl = document.getElementById('pressure');
+const uvEl = document.getElementById('uv');
+
+const airQuality = 'yes';
+
+const getWeather = city => {
+  fetch(
+    `http://api.weatherapi.com/v1/current.json?key=756b142720f34d1dad3184645231502&q=${city}&aqi=${airQuality}`
+  )
+    .then(response => response.json())
+    .then(data => {
+      townEl.innerHTML = data.location.name;
+      countryEl.innerHTML = data.location.country;
+      temperatureEl.innerHTML = `${data.current.temp_c}&deg C`;
+      conditionsEl.innerHTML = `<img src=${data.current.condition.icon}> <span>${data.current.condition.text}</span>`;
+      windEl.innerHTML = `wind: ${data.current.wind_kph} kph`;
+      pressureEl.innerHTML = `pressure: ${data.current.pressure_mb} hPa`;
+      uvEl.innerHTML = `index UV: ${data.current.uv}`;
+
+      console.log(data);
+    });
+};
+
+getWeather('Warsaw');
+
+searchButtonEl.addEventListener('click', e => {
+  e.preventDefault();
+  const city = searchInputEl.value;
+  getWeather(city);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'w-big': '380px',
+      },
+      height: {
+        'h-big': '380px',
+      },
+      colors: {
+        'black-opacity': '#000000d0',
+      },
+    },
   },
   plugins: [],
 };
